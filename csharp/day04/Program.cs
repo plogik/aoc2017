734401ec7a7fe4396fb7e4a10454df54caaa7383
Stefan Emanuelsson <stefan.emanuelsson@avensia.com>
2017-12-05 11:05:23 +0100

    Day 4 and 5, C#

---
+++ csharp/day04/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataEnum = File.ReadLines("../../inputs/day04.txt");
+            Console.WriteLine(SolvePt1(dataEnum));
+            Console.WriteLine(SolvePt2(dataEnum));
+        }
+
+        static int SolvePt1(IEnumerable<string> dataEnum)
+        {
+            return dataEnum
+                .Where(x => IsValid(x))
+                .Count();
+        }
+
+        static bool IsValid(string phrase)
+        {
+            var words = phrase
+                .Split(new[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries);
+            return words.Count() == words.Distinct().Count();
+        }
+
+        static int SolvePt2(IEnumerable<string> dataEnum)
+        {
+            return dataEnum
+                .Where(x => IsValidPt2(x))
+                .Count();
+        }
+
+        static bool IsValidPt2(string phrase)
+        {
+            var words = phrase
+                .Split(new[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => new string(x.ToArray().OrderBy(c => c).ToArray()))
+                .ToList();
+            return words.Count() == words.Distinct().Count();
+        }
+    }
+}
---
+++ csharp/day05/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllText("../../inputs/day05.txt");
+            var instructions = data
+                .Split(new[] {' ', '\n', '\r'}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+            Console.WriteLine(SolvePt1(instructions.ToArray())); // Make a copy so pt2 gets correct instructions!
+            Console.WriteLine(SolvePt2(instructions));
+        }
+
+        static int SolvePt1(int[] instructions)
+        {
+            var sp = 0;
+            var idx = 0;
+            while(sp < instructions.Length)
+            {
+                sp += instructions[sp]++;
+                idx++;
+            }
+            return idx; 
+        }
+
+        static int SolvePt2(int[] instructions)
+        {
+            var sp = 0;
+            var idx = 0;
+            while(sp < instructions.Length)
+            {
+                var prevSp = sp;
+                sp += instructions[sp];
+                instructions[prevSp] += instructions[prevSp] >= 3 ? -1 : 1;
+                idx++;
+            }
+            return idx; 
+        }
+    }
+}
