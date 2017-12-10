7c53fee47049c208dfe3a845fbc22711260c61c9
Stefan Emanuelsson <semanuelsson@gmail.com>
2017-12-10 20:07:36 +0100

    Alt day 9 Haskell

--- haskell/day09.hs
+++ haskell/day09.hs
@@ -21,10 +21,26 @@ solve s = go s False 0 0 0
             | otherwise = go xs inGb l t
                 (if inGb then (g + 1) else g)
 
+solve_v2 :: String -> (Int, Int)
+solve_v2 s = go s False 0 0 0
+    where go [] _ _ t g = (t, g)
+          go (x:xs) inGb l t g  =
+            case (x, inGb) of
+                ('{', True)  -> go xs inGb l t (g + 1)
+                ('{', False) -> go xs inGb (l + 1) t g
+                ('}', True)  -> go xs inGb l t (g + 1)
+                ('}', False) -> go xs inGb (l - 1) (t + l) g
+                ('!', _)     -> go (tail xs) inGb l t g
+                ('<', True)  -> go xs True l t (g + 1)
+                ('<', False) -> go xs True l t g
+                ('>', _)     -> go xs False l t g
+                (_, True)    -> go xs inGb l t (g + 1)
+                (_, False)   -> go xs inGb l t g
+
 main = do
     input <- readFile "../inputs/day09.txt"
     start <- getCPUTime
-    let result = solve input
+    let result = solve_v2 input
     end <- getCPUTime
     let diff = (fromIntegral (end - start)) / (10^12)
     printf "Total: %d, garbage: %d, time: %0.7fs\n" (fst result) (snd result) (diff::Double)
