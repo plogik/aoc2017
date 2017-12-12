97304afa60191125829dc5cc32afe89ea6b2a76c
Stefan Emanuelsson <semanuelsson@gmail.com>
2017-12-12 21:32:18 +0100

    Day 12 c#, not done

---
+++ csharp/day12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var data = File.ReadAllText("../../inputs/day12.txt");
+            var dataEnum = File.ReadLines("../../inputs/day12.txt");
+            Console.WriteLine(SolvePt1(dataEnum));
+            //Console.WriteLine("0 <-> 1,2,3".Split(" <-> ")[1]);
+        }
+
+        static int SolvePt1(IEnumerable<string> lines)
+        {
+            var  nodeData = lines
+                .Select(x => x.Split(" <-> "))
+                .ToDictionary(
+                        x => int.Parse(x[0]),
+                        x => x[1].Split(',')
+                            .Select(e => int.Parse(e))
+                            .ToList());
+            return nodeData[0][0];
+        }
+
+        static int LeafCount(Dictionary<int,List<int>> nodes, int root)
+        {
+        }
+    }
+}
